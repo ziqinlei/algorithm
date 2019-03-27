f55733250476b3f4bd0c99d5a22b6e9cc287cb85
leiziqin <ziqinlei@gmail.com>
2019-03-27 14:54:35 +0800

    更新 - 二叉树 - 判断一棵树是否是完全二叉树

--- src/com/ziqinlei/algorithm/tree/Tree_006_IsCBT.java
+++ src/com/ziqinlei/algorithm/tree/Tree_006_IsCBT.java
@@ -59,4 +59,51 @@ public class Tree_006_IsCBT {
         }
         return true;
     }
+
+    // for test -- print tree
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+    public static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            builder.append(space);
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.left = new Node(2);
+        head.right = new Node(6);
+        head.left.left = new Node(1);
+        head.left.right = new Node(5);
+        head.right.right = new Node(5);
+
+        printTree(head);
+        System.out.println(isCBT(head));
+
+        head.right.left = new Node(6);
+        head.left.left.left = new Node(1);
+        printTree(head);
+        System.out.println(isCBT(head));
+    }
 }
