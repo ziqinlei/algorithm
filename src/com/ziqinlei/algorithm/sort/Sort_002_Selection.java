31aafeafd0c14cee35317501fa73b3da96d02c4c
leiziqin <ziqinlei@gmail.com>
2019-05-02 21:03:31 +0800

    新增 - 排序 - 选择排序

---
+++ src/com/ziqinlei/algorithm/sort/Sort_002_Selection.java
@@ -0,0 +1,28 @@
+package com.ziqinlei.algorithm.sort;
+
+/**
+ * 选择排序
+ *
+ * @author Leiziqin
+ * @since 2019/5/2
+ */
+public class Sort_002_Selection {
+    public static void sort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            swap(a, i, min);
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
