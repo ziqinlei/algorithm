ad3feff0b67fbf35e8b7cc488e9b99f9bffc56d3
leiziqin <ziqinlei@gmail.com>
2019-05-03 10:16:59 +0800

    新增 - 排序 - 快速排序

---
+++ src/com/ziqinlei/algorithm/sort/Sort_006_QuickSort.java
@@ -0,0 +1,48 @@
+package com.ziqinlei.algorithm.sort;
+
+/**
+ * 快速排序
+ *
+ * @author Leiziqin
+ * @since 2019/5/3
+ */
+public class Sort_006_QuickSort {
+    public static void sort(int[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        int v = a[lo];
+        while (true) {
+            while (a[++i] < v) {
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (a[--j] > v) ;
+            if (i >= j) {
+                break;
+            }
+            swap(a, i, j);
+        }
+        swap(a, lo, j);
+        return j;
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
